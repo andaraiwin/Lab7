31b0b6cabb52e642bee295960ceb1678eb010f08
Raiwin Inthasit <andaraiwin@gmail.com>
2021-11-30 17:44:43 +0700

    Lab7_3 Completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,30 @@
 
 using namespace std;
 
+int adiff(int A, int B){
+  int C;
+    C = (A%360) - (B%360);
+        if(-C > 180){
+          return C = 180 - (-C%180);
+        }
+        if(C < 0){
+          return -C;
+        }
+        if(C >= 0){
+          return C;
+        }
+    return 0;
+}
+
+
 
 int main(){
   cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << "\n" << adiff(210,45);
+  cout << "\n" << adiff(0,360);
+  cout << "\n" << adiff(10,350);
+  cout << "\n" << adiff(95,260);
+  cout << "\n" << adiff(90,-90);
+  cout << "\n" << adiff(1000,280);
+  cout << "\n" << adiff(60,244);
 }
